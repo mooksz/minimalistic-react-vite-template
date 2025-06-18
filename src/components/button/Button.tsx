eb058a0e5c9610985002ed5b3c3c81678639b2b1
Mikey Haklander <mikey.haklander@capgemini.com>
2025-06-18 13:43:31 +0200

    feat(button component): add anchor element support

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,17 +1,32 @@
-import type { ButtonHTMLAttributes, ReactNode } from 'react';
+import type { AnchorHTMLAttributes, ButtonHTMLAttributes, ReactNode } from 'react';
 import styles from './Button.module.scss';
-import { clsx } from 'clsx';
+import { clsx, type ClassValue } from 'clsx';
 
-type ButtonProps = {
+type BaseProps = {
     children: ReactNode;
+    className?: ClassValue[];
+};
+
+type ButtonProps = BaseProps & {
+    tagName: 'button';
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button(props: Readonly<ButtonProps>) {
-    const { children } = props;
+type AnchorProps = BaseProps & {
+    tagName: 'a';
+} & AnchorHTMLAttributes<HTMLAnchorElement>;
 
-    return (
-        <button className={clsx(styles['button'])} type="button">
-            {children}
-        </button>
-    );
+type Props = ButtonProps | AnchorProps;
+
+export function Button(props: Readonly<Props>) {
+    const { tagName, className, ...rest } = props;
+
+    if (tagName === 'a') {
+        const anchorProps = rest as AnchorHTMLAttributes<HTMLAnchorElement>;
+        // eslint-disable-next-line jsx-a11y/anchor-has-content
+        return <a className={clsx(styles['button'], className)} {...anchorProps} />;
+    }
+
+    const buttonProps = rest as ButtonHTMLAttributes<HTMLButtonElement>;
+    // eslint-disable-next-line react-dom/no-missing-button-type
+    return <button className={clsx(styles['button'], className)} {...buttonProps} />;
 }
