686a2d33cf46e4438e72568fa007038530710728
Mikey Haklander <mikey.haklander@capgemini.com>
2025-06-18 11:15:21 +0200

    feat(main): add react-query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import '@/styles/vendor.scss';
 import App from '@/App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ZodError } from 'zod';
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: (failureCount, error) => {
+                // Disable retry for Zod validation errors
+                if (error instanceof ZodError) return false;
+                return failureCount < 3; // Retry up to 3 times for other errors
+            },
+        },
+    },
+});
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </StrictMode>,
 );
