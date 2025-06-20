6fda5e5c23f8415f3c768503c79a888308bd8165
Mikey Haklander <mikey.haklander@capgemini.com>
2025-06-20 14:03:02 +0200

    feat(commitlint): add wip type to commitlint rules

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,12 @@
+import { RuleConfigCondition, RuleConfigSeverity } from '@commitlint/types';
+
 export default {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            RuleConfigSeverity.Error,
+            'always',
+            ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'wip'],
+        ] as [RuleConfigSeverity, RuleConfigCondition, string[]],
+    },
 };
